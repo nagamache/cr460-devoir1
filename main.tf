ac88a4f72c784efaa1fabc39c3e5383b4909ea3f
Nicolas Gamache <nagamache@users.noreply.github.com>
2026-03-07 15:47:46 -0500

    fix: change VM size to Standard_B2s

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   name                = "vm-cr460"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  size                = "Standard_B1s"
+  size                = "Standard_B2s"
   admin_username      = "adminuser"
 
   network_interface_ids = [
