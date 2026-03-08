9df70fe787fe9b610c296a16518e33d2aa95d448
Nicolas Gamache <nagamache@users.noreply.github.com>
2026-03-08 12:10:26 -0400

    fix: use Standard_D2s_v3 VM size

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   name                = "vm-cr460"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  size                = "Standard_DS1_V2"
+  size                = "Standard_D2s_v3"
   admin_username      = "adminuser"
 
   network_interface_ids = [
